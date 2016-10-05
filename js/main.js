1ee57aa888127e9b4ee90a36bfe3fb99ac9ae021
Chloe Collier <achloecollier@gmail.com>
2016-10-04 21:25:59 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var token = '4d17e61fa3d6a5c5e6de6be4ffe48551f5c447bd';
+function userSearch() {
+  $.ajax({
+    url: "https://api.github.com/users/jisaacks/orgs?access_token=" + token,
+    dataType: "jsonp",
+    method: "GET",
+  }).done(function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+      CreateOrgs(response.data[i]);
+    }
+  });
+}
+
+function CreateOrgs(org) {
+  this.info = {
+    name: org.login,
+    image: org.avatar_url
+  },
+  this.makeOrgs = function() {
+    var container = $('<section>').attr('class', 'org-container');
+    var image = $('<img>').attr('src', this.info.image).appendTo(container);
+    var name = $('<h1>').html(this.info.name).appendTo(container);
+
+    $(container).appendTo('main').show().fadeIn(400);
+  };
+  this.makeOrgs();
+}
+
+$('nav').on('click', '#submit', function(event) {
+  event.preventDefault();
+  userSearch();
+});
